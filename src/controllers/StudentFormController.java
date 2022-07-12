23faf8ab0a46a5eeeacc0d1602b6d76627aa7cfb
Kdulanjan <kalananil19@gmail.com>
2022-07-12 10:14:45 +0530

    Create DB Connection

---
+++ src/controllers/StudentFormController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class StudentFormController {
+    public TextField txtId;
+    public TextField txtNAme;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtNIC;
+    public Button btnSaveStudent;
+    public Button btnDelete;
+    public Button btnNew;
+    public TableView tblStudent;
+    public TableColumn RID;
+    public TableColumn RNAme;
+    public TableColumn REmail;
+    public TableColumn RContact;
+    public TableColumn RAddress;
+    public TableColumn RNic;
+
+    public void textFeild_key_releseed2(KeyEvent keyEvent) {
+    }
+
+    public void HOnAction(ActionEvent actionEvent) {
+    }
+
+    public void NewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SaveOnAction(ActionEvent actionEvent) {
+    }
+}
